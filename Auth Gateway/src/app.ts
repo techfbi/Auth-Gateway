8026eec30b083267bfb314437fcf1282d99f9c56
Tech_fbi <oluwafemijohnson347@gmail.com>
2026-07-28 05:55:19 +0100

    vercel json

--- Auth Gateway/src/app.ts
+++ Auth Gateway/src/app.ts
@@ -3,9 +3,9 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
-import { env } from "./src/config/env.js";
-import { errorHandler } from "./src/middleware/errorHandler.js";
-import { authRouter } from "./src/auth module/auth.routes.js";
+import { env } from "./config/env.js";
+import { errorHandler } from "./middleware/errorHandler.js";
+import { authRouter } from "./auth module/auth.routes.js";
 
 export const app = express();
 
